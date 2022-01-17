cce2088c711407fab85df1dbbc9174bafb58513b
Lucas Souza <lucascorreia325@gmail.com>
2022-01-17 09:42:26 -0300

    Adição do componente de fotos

---
+++ src/components/userPicture/index.jsx
@@ -0,0 +1,12 @@
+import {
+    Container,
+    ProfilePicture
+} from './style';
+
+const UserPicture = props => (
+    <Container>
+        <ProfilePicture src={props.url} alt={props.alternativeText} />
+    </Container>
+);
+
+export default UserPicture; 
