ba716529bca79d2986802fc3655afe2b1f6e4b69
Lucas Souza <lucascorreia325@gmail.com>
2022-01-17 09:25:14 -0300

    Mudanças no escopo e adição de novos estilos

--- src/components/container/styles.js
+++ src/components/container/styles.js
@@ -3,10 +3,26 @@ import styled from 'styled-components';
 export const Container = styled.main`
     width: 100%;
     height: 100vh;
-    background: linear-gradient(120deg, #252525, #444);
+    background: linear-gradient(45deg,#ff5e00,#d18b5a, #b3b0b0,#6888a3,#6762d1d6);
+    background-size: 300% 300%;
+    animation: colors 12s ease infinite;
 
     display: flex;
     align-items: center;
     justify-content: center;
     flex-direction: column;
+
+    @keyframes colors {
+        0% {
+            background-position: 0% 50%;
+        }
+    
+        50% {
+            background-position: 100% 50%;
+        }
+    
+        100% {
+            background-position: 0% 50%;
+        }
+    }
 `
